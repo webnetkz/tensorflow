3ad0c7b9c39bacdc6b951bf739d758f558d0b679
webnetkz <dbqqbq@gmail.com>
2021-12-04 03:38:09 +0600

    up

--- fashion.py
+++ fashion.py
@@ -1,6 +1,8 @@
+# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow import keras
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labesl) = fashion_mnist.load_data()
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
 
+print(tf.__version__)
